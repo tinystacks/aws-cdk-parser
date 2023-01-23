e852d2723c362d8ee4ab1859cb4c2c8415509d86
Caleb Courier <caleb.courier@gmail.com>
2023-01-23 09:33:00 -0600

    update resource definition source

--- src/s3.ts
+++ src/s3.ts
@@ -1,6 +1,7 @@
 import { CdkDiff, Json } from '@tinystacks/predeploy-infra';
 
 // https://docs.aws.amazon.com/AmazonS3/latest/API/API_Bucket.html
+// https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetBucketTagging.html#AmazonS3-GetBucketTagging-response-TagSet
 function parseS3Bucket (diff: CdkDiff, cloudformationTemplate: Json): Json {
   const { logicalId } = diff;
   const [ _logicalId, cfnEntry = {} ] = Object.entries<Json>(cloudformationTemplate.Resources).find(([key]) => key === logicalId) || [];
